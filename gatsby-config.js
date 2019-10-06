d97045d048b173fa6bd7318ce825d3ed846b3046
yokomotod <yokomotod@gmail.com>
2019-10-06 18:55:27 +0900

    add PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,21 @@ module.exports = {
         trackingId: "UA-51089249-5",
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `yokomotod.dev`,
+        short_name: `yokomotod.dev`,
+        start_url: `/`,
+        background_color: `#007acc`,
+        theme_color: `#007acc`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.jpg`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
   // Customize your site metadata:
   siteMetadata: {
