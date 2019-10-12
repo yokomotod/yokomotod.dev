11949237f9bf7ec1aca94a2fec60ce64a16f0de4
yokomotod <yokomotod@gmail.com>
2019-10-12 17:02:28 +0900

    change manifest display: minimal-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         theme_color: `#007acc`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
+        display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
