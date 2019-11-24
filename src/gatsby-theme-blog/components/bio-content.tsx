1aaf323fcd6eea74678a7a05cbf80cb929373b50
yokomotod <yokomotod@gmail.com>
2019-11-24 22:12:19 +0900

    upgrade gatsby-theme-blog

--- src/gatsby-theme-blog/components/bio-content.tsx
+++ src/gatsby-theme-blog/components/bio-content.tsx
@@ -2,10 +2,19 @@ import React, { Fragment } from "react"
 import { Styled } from "theme-ui"
 import { useStaticQuery, graphql } from "gatsby"
 
-// keep .js due to https://github.com/gatsbyjs/gatsby/issues/18554
-
-export default () => {
-  const data = useStaticQuery(
+type Data = {
+  site: {
+    siteMetadata: {
+      author: string
+      social: {
+        url: string
+        name: string
+      }[]
+    }
+  }
+}
+const BioContent: React.FC = () => {
+  const data = useStaticQuery<Data>(
     graphql`
       query {
         site {
@@ -45,3 +54,5 @@ export default () => {
     </Fragment>
   )
 }
+
+export default BioContent
