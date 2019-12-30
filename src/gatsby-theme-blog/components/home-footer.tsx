7aa94cc6368967e00bbe2697932087d87db91afd
yokomotod <yokomotod@gmail.com>
2019-12-30 20:06:45 +0900

    follow gatsby-theme-blog home-footer fix

--- src/gatsby-theme-blog/components/home-footer.tsx
+++ src/gatsby-theme-blog/components/home-footer.tsx
@@ -1,35 +1,24 @@
+import { css } from "@styled-system/css"
 import React from "react"
-import { useColorMode } from "theme-ui"
-import defaultThemeColors from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/colors"
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { css } = require("theme-ui")
+import { Styled } from "theme-ui"
 
-const Footer: React.FC = () => {
-  const [colorMode] = useColorMode()
-
-  return (
-    <footer
-      css={css({
-        mt: 4,
-        pt: 3,
-      })}
+const Footer: React.FC = () => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    © {new Date().getFullYear()}, Powered by
+    {` `}
+    <Styled.a
+      href="https://www.gatsbyjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      © {new Date().getFullYear()}, Powered by
-      {` `}
-      <a
-        css={{
-          color:
-            colorMode === "dark"
-              ? defaultThemeColors.modes.dark.text
-              : defaultThemeColors.text,
-        }}
-        href="https://www.gatsbyjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Gatsby
-      </a>
-    </footer>
-  )
-}
+      Gatsby
+    </Styled.a>
+  </footer>
+)
+
 export default Footer
